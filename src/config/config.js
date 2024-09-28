129a767e1bc0f3593e5e514223156916cd2753da
hasnain.khurram@yavr.io==Hasnain Khurram <hasnain.khurram2019@gmail.com>
2024-09-29 00:26:59 +0500

    Initialize Project Structure

---
+++ src/config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  development: {
+    username: "postgres",
+    password: "yourPassword",
+    database: "Apna_Garage",
+    host: "127.0.0.1",
+    dialect: "postgres",
+  },
+};
