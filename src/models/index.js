f7039782278805c6bc6fb0c5757653a8952cd81f
hasnain <hasnain.khurram2019@gmail.com>
2024-09-29 00:26:59 +0500

    Initialize Project Structure

---
+++ src/models/index.js
@@ -0,0 +1,35 @@
+const Sequelize = require("sequelize");
+const config = require("../config/config.js").development;
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  {
+    host: config.host,
+    dialect: config.dialect,
+  }
+);
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.Car = require("./sample.js")(db.sequelize, db.Sequelize.DataTypes);
+
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize
+  .sync()
+  .then(() => {
+    console.log("Database working");
+  })
+  .catch((error) => {
+    console.error("Error creating database:", error);
+  });
+
+module.exports = db;
