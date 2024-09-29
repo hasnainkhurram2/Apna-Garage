fca8d1e812a998614bdeddfe4ec1dce2864c4c66
Farooq Haider <farooqhaider2323@gmail.com>
2024-09-29 19:08:11 +0500

    Implement Service Model

--- src/models/index.js
+++ src/models/index.js
@@ -15,7 +15,8 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-db.Car = require("./sample.js")(db.sequelize, db.Sequelize.DataTypes);
+//db.Car = require("./sample.js")(db.sequelize, db.Sequelize.DataTypes);
+db.Service = require("./services.js")(db.sequelize, db.Sequelize.DataTypes);
 
 Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
