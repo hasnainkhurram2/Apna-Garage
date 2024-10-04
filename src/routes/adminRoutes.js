2802baeff05fc99773d978fa5aa42193dbfe1d15
Farooq Haider <farooqhaider2323@gmail.com>
2024-10-04 13:27:10 +0500

    Implement Admin Routes Farooq

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -8,7 +8,6 @@ router.route('/users/:id/accept').put(adminController.acceptWorker);
 
 router.route('/users/:id/reject').delete(adminController.rejectWorker);
 
-router.route("users/:id/delete").delete(adminController.deleteAnyUser);
-
+router.route('/feedbacks').get(adminController.getAllFeedbacks);
 
 module.exports = router;
