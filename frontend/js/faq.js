3d0476173d20bf3f1fac4a991e1cb6f37dc7cd65
naailmahmood <naail.r.mahmood@gmail.com>
2024-11-08 19:59:17 +0500

    Implemented FAQ page with 5 questions, also made changes to css of landingPage, provideFeedback pages

---
+++ frontend/js/faq.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const questions = document.querySelectorAll(".faq-question");
+
+    questions.forEach(question => {
+        question.addEventListener("click", () => {
+            const answer = question.nextElementSibling;
+            const toggleBtn = question.querySelector(".toggle-btn");
+            
+            if (answer.style.display === "block") {
+                answer.style.display = "none";
+                toggleBtn.textContent = "+";
+            } else {
+                answer.style.display = "block";
+                toggleBtn.textContent = "-";
+            }
+        });
+    });
+});
