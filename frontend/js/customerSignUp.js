a0f9446949f51c11926d3316c03692c21df498e2
M-Abdullah-Azeem <m.abdullah.azeem.official@gmail.com>
2024-11-15 22:18:20 +0500

    Implement Customer and Technician Sign Up Frontend with Backend Connectivity.

--- frontend/js/customerSignUp.js
+++ frontend/js/customerSignUp.js
@@ -1,30 +1,60 @@
 // Redirect to signUpSegregation.html
 function redirectToSignUpSegregation() {
-    window.location.href = "../pages/signUpSegregation.html";
+  window.location.href = '../pages/signUpSegregation.html';
+}
+
+// Form validation
+const signUpForm = document.getElementById('signUpForm');
+
+signUpForm.addEventListener('submit', async function (e) {
+  e.preventDefault(); // Prevent form submission
+
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  const confirmPassword = document.getElementById('confirmPassword').value;
+  const dob = document.getElementById('dob').value;
+  const address = document.getElementById('address').value;
+  const contact = document.getElementById('contact').value;
+
+  // Simple validation
+  if (password !== confirmPassword) {
+    alert('Passwords do not match!');
+    return;
   }
-  
-  // Form validation
-  const signUpForm = document.getElementById('signUpForm');
-  
-  signUpForm.addEventListener('submit', function (e) {
-    e.preventDefault(); // Prevent form submission
-  
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    const confirmPassword = document.getElementById('confirmPassword').value;
-    const dob = document.getElementById('dob').value;
-    const address = document.getElementById('address').value;
-    const contact = document.getElementById('contact').value;
-  
-    // Simple validation
-    if (password !== confirmPassword) {
-      alert("Passwords do not match!");
-      return;
+
+  const customerData = {
+    email,
+    password,
+    dob,
+    address,
+    contact,
+  };
+  try {
+    const response = await fetch(
+      'http://localhost:3000/api/v1/customers/signup',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(customerData),
+      }
+    );
+    const result = await response.json();
+    if (response.ok) {
+      if (
+        confirm(
+          'Customer Sign Up successful! Press OK to proceed to your Dashboard.'
+        )
+      ) {
+        window.location.href = './customerDashboard.html';
+      }
+    } else {
+      alert(`Oops, Something went wrong. Try Again Later.`);
+      console.log(`Error while trying to Sign Up: ${result.message}`);
     }
-  
-    // Success message
-    alert("Sign Up Successful!");
-    // Redirect or clear the form if needed
-    signUpForm.reset();
-  });
-  
+  } catch (error) {
+    alert(`Oops, Something went wrong. Try Again Later.`);
+    console.log(`Error while trying to Sign Up: ${result.message}`);
+  }
+});
--- src/models/User.js
+++ src/models/User.js
@@ -13,10 +13,6 @@ module.exports = (sequelize) => {
         primaryKey: true,
         autoIncrement: true,
       },
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
       email: {
         type: DataTypes.STRING,
         allowNull: false,
