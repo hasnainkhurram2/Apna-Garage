c9650f5131b0028236b7249e9aceb5c59a77f2e1
naailmahmood <naail.r.mahmood@gmail.com>
2024-11-23 00:34:23 +0500

    Implemented functionality that technicians will only be able to see requests according to their expertise

--- src/controller/technicianController.js
+++ src/controller/technicianController.js
@@ -36,35 +36,69 @@ exports.signUp = async (req, res) => {
   }
 };
 
-
 exports.requestsMadeByCustomers = async (req, res) => {
   try {
-    console.log('Processing customer requests');
+    const technicianId = req.session.userDetails.userId;
+
+    const query1 = `
+      SELECT t.type
+      FROM "Technician" t
+      JOIN "User" u ON u.id = t.user_id
+      WHERE t.user_id = :technicianId
+    `;
+  
+    const result = await models.sequelize.query(query1, {
+      type: models.Sequelize.QueryTypes.SELECT,
+      replacements: { technicianId }, // Parameter binding
+    });
+  
+    const technicianType = result[0]?.type; // Access the first row's 'type'
+    let technician = '';
+    if (technicianType === 'mechanic')
+    {
+      technician = '4';
+    }
+    if (technicianType === 'fuelSupplier')
+    {
+      technician = '3';
+    }
+    if (technicianType === 'electrician')
+    {
+      technician = '2';
+    }
+    if (technicianType === 'bodyWorker')
+    {
+      technician = '1';
+    }
+
     const query = `
-      SELECT 
-        r.completed AS completed, 
-        r.description AS description, 
-        r."startTime" AS "startTime", 
-        s.name AS name, 
-        u.name AS requestingUser,
-        r.id AS id
-      FROM 
-        "Request" AS r 
-      JOIN 
-        "Service" AS s 
-      ON 
-        r.service_id = s.id 
-      JOIN 
-        "User" AS u 
-      ON 
-        r.requesting_user_id = u.id 
-      ORDER BY 
-        r."startTime" DESC;
+    SELECT 
+    r.completed AS completed, 
+    r.description AS description, 
+    r."startTime" AS "startTime", 
+    s.name AS name, 
+    u.name AS requestingUser,
+    r.id AS id
+  FROM 
+    "Request" AS r 
+  JOIN 
+    "Service" AS s 
+  ON 
+    r.service_id = s.id 
+  JOIN 
+    "User" AS u 
+  ON 
+    r.requesting_user_id = u.id 
+  WHERE 
+    s."providerType" = :technician
+  ORDER BY 
+    r."startTime" DESC;  
     `;
 
     console.log('Executing query...');
     const data = await models.sequelize.query(query, {
       type: models.Sequelize.QueryTypes.SELECT,
+      replacements : {technician},
     });
 
     if (!data || data.length === 0) {
